25b5d9647885f5d2996b65b8b1daacc0f7ac1ff9
Piers <piers.barber@logicmonkey.co.uk>
2015-08-03 19:02:38 +0100

    fixed the mathmodcollection file for the trefoil knot

--- mathmod/lm_trefoil_knot/mathmodcollection.js
+++ mathmod/lm_trefoil_knot/mathmodcollection.js
@@ -1,35 +1,39 @@
 {
-    "Param3D": {
-        "Component": [
-            "Trefoil Knot"
-        ],
-        "Description": [
-            "Two trips round a torus while going up and down three times"
-        ],
-        "Fx": [
-            "(6*(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*sin(3*u)*sin(v))/(sqrt(36*(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2*\nsin(3*u)^2+36*(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))^2*sin(3*u)^2+((-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*\n(8*cos(2*u)*(sin(3*u)/3+1)+4*sin(2*u)*cos(3*u))-(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2)^(2)))+\n((4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))*cos(v))/sqrt((4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2+(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))^2)+4*sin(2*u)*(sin(3*u)/3+1)"
-        ],
-        "Fy": [
-            "-(6*(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))*sin(3*u)*sin(v))/(sqrt(36*(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2*\nsin(3*u)^2+36*(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))^2*sin(3*u)^2+((-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*\n(8*cos(2*u)*(sin(3*u)/3+1)+4*sin(2*u)*cos(3*u))-(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2)^(2)))+\n((-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*cos(v))/sqrt((4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2+(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))^2)+4*cos(2*u)*(sin(3*u)/3+1)"
-        ],
-        "Fz": [
-            "(((-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*(8*cos(2*u)*(sin(3*u)/3+1)+4*sin(2*u)*cos(3*u))-\n(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2)*sin(v))/(sqrt(36*(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2*sin(3*u)^2+36*\n(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))^2*sin(3*u)^2+((-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*\n(8*cos(2*u)*(sin(3*u)/3+1)+4*sin(2*u)*cos(3*u))-(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2)^(2)))+2*cos(3*u)"
-        ],
-        "Name": [
-            "Trefoil"
-        ],
-        "Umax": [
-            "2*pi"
-        ],
-        "Umin": [
-            "0"
-        ],
-        "Vmax": [
-            "2*pi"
-        ],
-        "Vmin": [
-            "0"
-        ]
-    }
+"MathModels":[
+   {
+       "Param3D": {
+           "Component": [
+               "Trefoil Knot"
+           ],
+           "Description": [
+               "Two trips round a torus while going up and down three times"
+           ],
+           "Fx": [
+               "(6*(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*sin(3*u)*sin(v))/(sqrt(36*(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2*\nsin(3*u)^2+36*(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))^2*sin(3*u)^2+((-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*\n(8*cos(2*u)*(sin(3*u)/3+1)+4*sin(2*u)*cos(3*u))-(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2)^(2)))+\n((4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))*cos(v))/sqrt((4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2+(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))^2)+4*sin(2*u)*(sin(3*u)/3+1)"
+           ],
+           "Fy": [
+               "-(6*(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))*sin(3*u)*sin(v))/(sqrt(36*(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2*\nsin(3*u)^2+36*(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))^2*sin(3*u)^2+((-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*\n(8*cos(2*u)*(sin(3*u)/3+1)+4*sin(2*u)*cos(3*u))-(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2)^(2)))+\n((-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*cos(v))/sqrt((4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2+(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))^2)+4*cos(2*u)*(sin(3*u)/3+1)"
+           ],
+           "Fz": [
+               "(((-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*(8*cos(2*u)*(sin(3*u)/3+1)+4*sin(2*u)*cos(3*u))-\n(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2)*sin(v))/(sqrt(36*(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2*sin(3*u)^2+36*\n(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))^2*sin(3*u)^2+((-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*\n(8*cos(2*u)*(sin(3*u)/3+1)+4*sin(2*u)*cos(3*u))-(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2)^(2)))+2*cos(3*u)"
+           ],
+           "Name": [
+               "Trefoil"
+           ],
+           "Umax": [
+               "2*pi"
+           ],
+           "Umin": [
+               "0"
+           ],
+           "Vmax": [
+               "2*pi"
+           ],
+           "Vmin": [
+               "0"
+           ]
+       }
+   }
+]
 }
 
