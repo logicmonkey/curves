6859d7e8ac3f09c7808ad597ad242eec439b1be0
Piers <piers.barber@logicmonkey.co.uk>
2015-08-04 01:07:00 +0100

    adding lazy v parametrization to simplify equations a little

---
+++ mathmod/lm_trefoil_lazy/mathmodcollection.js
@@ -0,0 +1,39 @@
+{
+"MathModels":[
+   {
+       "Param3D": {
+           "Component": [
+               "Trefoil Knot (lazy)"
+           ],
+           "Description": [
+               "Two trips round a torus while going up and down three times"
+           ],
+           "Fx": [
+"(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))*cos(v)/sqrt((4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2+(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))^2)+4*sin(2*u)*(sin(3*u)/3+1)"
+           ],
+           "Fy": [
+"(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*cos(v)/sqrt((4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2+(-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))^2)+4*cos(2*u)*(sin(3*u)/3+1)"
+           ],
+           "Fz": [
+"((-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*(8*cos(2*u)*(sin(3*u)/3+1)+4*sin(2*u)*cos(3*u))-(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2)*sin(v)/abs((-8*cos(2*u)*(sin(3*u)/3+1)-4*sin(2*u)*cos(3*u))*(8*cos(2*u)*(sin(3*u)/3+1)+4*sin(2*u)*cos(3*u))-(4*cos(2*u)*cos(3*u)-8*sin(2*u)*(sin(3*u)/3+1))^2)+2*cos(3*u)"
+           ],
+           "Name": [
+               "Trefoil Knot (lazy)"
+           ],
+           "Umax": [
+               "2*pi"
+           ],
+           "Umin": [
+               "0"
+           ],
+           "Vmax": [
+               "2*pi"
+           ],
+           "Vmin": [
+               "0"
+           ]
+       }
+   }
+]
+}
+
