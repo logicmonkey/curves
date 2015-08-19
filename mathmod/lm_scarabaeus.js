4b92bafed2af34211fa6d889b78cd0255555b46f
Piers <piers.barber@logicmonkey.co.uk>
2015-08-19 22:53:43 +0100

    mathmod now supports file saves!

---
+++ mathmod/lm_scarabaeus.js
@@ -0,0 +1,38 @@
+{
+    "MathModels": [
+        {
+            "Param3D": {
+                "Component": [
+                    "Scarabaeus"
+                ],
+                "Description": [
+                    ""
+                ],
+                "Fx": [
+                    "-(2.4*(4*cos(u)*sin(2*u)+2*sin(u)*cos(2*u)-6*cos(u)*sin(u))*cos(3*u)*sin(v))/(sqrt(36*(-4*sin(u)*sin(2*u)+2*cos(u)*cos(2*u)+3*sin(u)^2-3*cos(u)^2)^2*cos(3*u)^2+36*(4*cos(u)*sin(2*u)+2*sin(u)*cos(2*u)-6*cos(u)*sin(u))^2*cos(3*u)^2+((-4*cos(u)*sin(2*u)-2*sin(u)*cos(2*u)+6*cos(u)*sin(u))*(4*cos(u)*sin(2*u)+2*sin(u)*cos(2*u)-6*cos(u)*sin(u))-(-4*sin(u)*sin(2*u)+2*cos(u)*cos(2*u)+3*sin(u)^2-3*cos(u)^2)^2)^(2)))+(0.4*(-4*sin(u)*sin(2*u)+2*cos(u)*cos(2*u)+3*sin(u)^2-3*cos(u)^2)*cos(v))/sqrt((-4*sin(u)*sin(2*u)+2*cos(u)*cos(2*u)+3*sin(u)^2-3*cos(u)^2)^2+(4*cos(u)*sin(2*u)+2*sin(u)*cos(2*u)-6*cos(u)*sin(u))^2)+2*cos(u)*cos(2*u)-3*cos(u)^2"
+                ],
+                "Fy": [
+                    "(2.4*(-4*sin(u)*sin(2*u)+2*cos(u)*cos(2*u)+3*sin(u)^2-3*cos(u)^2)*cos(3*u)*sin(v))/(sqrt(36*(-4*sin(u)*sin(2*u)+2*cos(u)*cos(2*u)+3*sin(u)^2-3*cos(u)^2)^2*cos(3*u)^2+36*(4*cos(u)*sin(2*u)+2*sin(u)*cos(2*u)-6*cos(u)*sin(u))^2*cos(3*u)^2+((-4*cos(u)*sin(2*u)-2*sin(u)*cos(2*u)+6*cos(u)*sin(u))*(4*cos(u)*sin(2*u)+2*sin(u)*cos(2*u)-6*cos(u)*sin(u))-(-4*sin(u)*sin(2*u)+2*cos(u)*cos(2*u)+3*sin(u)^2-3*cos(u)^2)^2)^(2)))+(0.4*(4*cos(u)*sin(2*u)+2*sin(u)*cos(2*u)-6*cos(u)*sin(u))*cos(v))/sqrt((-4*sin(u)*sin(2*u)+2*cos(u)*cos(2*u)+3*sin(u)^2-3*cos(u)^2)^2+(4*cos(u)*sin(2*u)+2*sin(u)*cos(2*u)-6*cos(u)*sin(u))^2)+2*sin(u)*cos(2*u)-3*cos(u)*sin(u)"
+                ],
+                "Fz": [
+                    "(0.4*((-4*cos(u)*sin(2*u)-2*sin(u)*cos(2*u)+6*cos(u)*sin(u))*(4*cos(u)*sin(2*u)+2*sin(u)*cos(2*u)-6*cos(u)*sin(u))-(-4*sin(u)*sin(2*u)+2*cos(u)*cos(2*u)+3*sin(u)^2-3*cos(u)^2)^2)*sin(v))/(sqrt(36*(-4*sin(u)*sin(2*u)+2*cos(u)*cos(2*u)+3*sin(u)^2-3*cos(u)^2)^2*cos(3*u)^2+36*(4*cos(u)*sin(2*u)+2*sin(u)*cos(2*u)-6*cos(u)*sin(u))^2*cos(3*u)^2+((-4*cos(u)*sin(2*u)-2*sin(u)*cos(2*u)+6*cos(u)*sin(u))*(4*cos(u)*sin(2*u)+2*sin(u)*cos(2*u)-6*cos(u)*sin(u))-(-4*sin(u)*sin(2*u)+2*cos(u)*cos(2*u)+3*sin(u)^2-3*cos(u)^2)^2)^(2)))+2*sin(3*u)"
+                ],
+                "Name": [
+                    "Scarabaeus"
+                ],
+                "Umax": [
+                    "2*pi"
+                ],
+                "Umin": [
+                    "0"
+                ],
+                "Vmax": [
+                    "2*pi"
+                ],
+                "Vmin": [
+                    "0"
+                ]
+            }
+        }
+    ]
+}
